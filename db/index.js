f881a69769206f322de85ab970916506973a21f6
jingqu <1264352733@qq.com>
2022-01-15 22:50:30 +0800

    连接数据库

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+const { host, user, password, database } = require("../config/cfg.json");
+
+const db = mysql.createPool({
+  host,
+  user,
+  password,
+  database,
+});
+
+// 测试 mysql 是否连接正常
+db.query("select * from ev_users", (err, result) => {
+  if (err) {
+    console.log(err.message);
+    return err;
+  }
+  console.log(result);
+});
